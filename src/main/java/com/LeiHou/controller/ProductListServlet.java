058d9e11c7206d6276a211a34cac4385f2bc9da0
rs-1108leihou <2950256840@qq.com>
2022-05-18 11:10:34 +0800

    week13

--- src/main/java/com/LeiHou/controller/ProductListServlet.java
+++ src/main/java/com/LeiHou/controller/ProductListServlet.java
@@ -1,17 +1,34 @@
 package com.LeiHou.controller;
 
+import com.LeiHou.dao.ProductDao;
+import com.LeiHou.model.Product;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.List;
 
 @WebServlet(name = "/admin/productList",value = "/admin/productList")
 public class ProductListServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        try{
+            ProductDao productDao = new ProductDao();
+            Connection con ;
+            con = (Connection) getServletContext().getAttribute("con");
+            List<Product> productList = productDao.findAll(con);
+            request.setAttribute("productList",productList);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
         String path="/WEB-INF/views/admin/productList.jsp";
         request.getRequestDispatcher(path).forward(request,response);
+
     }
 
 
